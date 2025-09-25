ca62d19f5defe75af25f44a4dd3e8310ba3a8a65
subin1511 <subinkrishnana005@gmail.com>
2025-09-26 02:31:52 +0530

    Deploy Flutter web app to GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9a37f02deca34492da0f46b56fc33a8e
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "7ed47e849fac1e79dbb6a5d2a0de7781",
+"flutter_bootstrap.js": "7bd86912e2e5d6e1e58fe2e83cdac529",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
