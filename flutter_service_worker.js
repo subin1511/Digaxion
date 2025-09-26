854dde23e8b1be571ef94705be18a4f3a9477721
Subin Krishnan <126809043+subin1511@users.noreply.github.com>
2025-09-26 14:29:59 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "da5666086cd28368cff96a78eb09d175
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "64d90e9c0f21cdc860af2f79cadd6412",
+"flutter_bootstrap.js": "30d019a38d37fa7d599ebc44ec6bef60",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
